1f7748de5036abd527715007265837b8b64bbf68
Shengkang Duan <dsk021112@yeah.net>
2024-04-10 14:23:27 +0800

    Add test program

---
+++ libmatrix/libmatrixTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  libmatrixTest
+//
+//  Created by Shengkang Duan on 10/4/2024.
+//
+
+#import <Foundation/Foundation.h>
+#import "libmatrix.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
