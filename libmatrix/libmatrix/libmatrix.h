e5fd3cf9df814301cacf393733d026b804289b89
Shengkang Duan <dsk021112@yeah.net>
2024-06-07 11:02:56 +0800

    Update method name

--- libmatrix/libmatrix/libmatrix.h
+++ libmatrix/libmatrix/libmatrix.h
@@ -5,7 +5,7 @@
 //  Created by dsk502 on 2/2/2024.
 //
 
-//#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for libmatrix.
 FOUNDATION_EXPORT double libmatrixVersionNumber;
--- libmatrix/libmatrixTest/main.m
+++ libmatrix/libmatrixTest/main.m
@@ -10,7 +10,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        int data1[] = {1,2,3,4,5,6,7,8,9};
+        IntMatrix *matrix1 = [[IntMatrix alloc] initWithRows:3 Columns:3 Numbers:data1];
+        int data2[] = {4,5,3,6,8,9,7,5,2};
+        IntMatrix *matrix2 = [[IntMatrix alloc] initWithRows:3 Columns:3 Numbers:data2];
+        IntMatrix *matrix3 = [matrix1 add:matrix2];
     }
     return 0;
 }
